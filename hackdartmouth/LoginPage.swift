5ee457958296db02815f99bf6f76d298527da6f5
Yuvi0044 <goenkayuvaraj@gmail.com>
2025-04-26 23:12:54 -0400

    Update LoginPage.swift

--- hackdartmouth/LoginPage.swift
+++ hackdartmouth/LoginPage.swift
@@ -63,7 +63,7 @@ struct LoginPage: View {
             .foregroundColor(.blue)
             .padding(.top, 10)
             .sheet(isPresented: $showCreateAccount) {
-                CreateAccountPage
+                CreateAccountPage()
                     .environmentObject(userSession)
             }
 
@@ -125,3 +125,4 @@ struct LoginResponse: Codable {
     var status: String
     var message: String
 }
+
